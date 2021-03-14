bed42fc9210ee801b619d67e93cde2bda48cc6e9
scCoded <clark.sophie2000@gmail.com>
2021-03-14 13:15:00 +0000

    CA-4: Added ingredients parameter (with measurements)

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ class App extends React.Component{
   state = {
     nameOfDrink: undefined,
     method: undefined,
+    ingredients:undefined,
     photo: undefined, 
     error: undefined
   }
@@ -23,6 +24,23 @@ class App extends React.Component{
         this.setState({
           nameOfDrink : process_data.drinks[0].strDrink,
           method : process_data.drinks[0].strInstructions,
+          ingredients: [
+            [process_data.drinks[0].strIngredient1, process_data.drinks[0].strMeasure1], 
+            [process_data.drinks[0].strIngredient2, process_data.drinks[0].strMeasure2], 
+            [process_data.drinks[0].strIngredient3, process_data.drinks[0].strMeasure3],
+            [process_data.drinks[0].strIngredient4, process_data.drinks[0].strMeasure4], 
+            [process_data.drinks[0].strIngredient5, process_data.drinks[0].strMeasure5],
+            [process_data.drinks[0].strIngredient6, process_data.drinks[0].strMeasure6], 
+            [process_data.drinks[0].strIngredient7, process_data.drinks[0].strMeasure7],
+            [process_data.drinks[0].strIngredient8, process_data.drinks[0].strMeasure8], 
+            [process_data.drinks[0].strIngredient9, process_data.drinks[0].strMeasure9], 
+            [process_data.drinks[0].strIngredient10, process_data.drinks[0].strMeasure10], 
+            [process_data.drinks[0].strIngredient11, process_data.drinks[0].strMeasure11], 
+            [process_data.drinks[0].strIngredient12, process_data.drinks[0].strMeasure12], 
+            [process_data.drinks[0].strIngredient13, process_data.drinks[0].strMeasure13],
+            [process_data.drinks[0].strIngredient14, process_data.drinks[0].strMeasure14],
+            [process_data.drinks[0].strIngredient15, process_data.drinks[0].strMeasure15], 
+          ],
           photo : process_data.drinks[0].strDrinkThumb,
           error : ""
         });
@@ -31,8 +49,9 @@ class App extends React.Component{
         this.setState({
           nameOfDrink : undefined,
           method : undefined,
+          ingredients:undefined,
           photo: undefined,
-          error : "Please enter the name of thedrink..."
+          error : "Please enter the name of the drink..."
         });
       }
       
@@ -41,6 +60,7 @@ class App extends React.Component{
       this.setState({
         nameOfDrink : undefined,
         method : undefined,
+        ingredients:undefined,
         photo : undefined,
         error : "The name you entered was not found..."
       });
@@ -56,6 +76,23 @@ class App extends React.Component{
     this.setState({
           nameOfDrink : process_data.drinks[0].strDrink,
           method : process_data.drinks[0].strInstructions,
+          ingredients: [
+            [process_data.drinks[0].strIngredient1, process_data.drinks[0].strMeasure1], 
+            [process_data.drinks[0].strIngredient2, process_data.drinks[0].strMeasure2], 
+            [process_data.drinks[0].strIngredient3, process_data.drinks[0].strMeasure3],
+            [process_data.drinks[0].strIngredient4, process_data.drinks[0].strMeasure4], 
+            [process_data.drinks[0].strIngredient5, process_data.drinks[0].strMeasure5],
+            [process_data.drinks[0].strIngredient6, process_data.drinks[0].strMeasure6], 
+            [process_data.drinks[0].strIngredient7, process_data.drinks[0].strMeasure7],
+            [process_data.drinks[0].strIngredient8, process_data.drinks[0].strMeasure8], 
+            [process_data.drinks[0].strIngredient9, process_data.drinks[0].strMeasure9], 
+            [process_data.drinks[0].strIngredient10, process_data.drinks[0].strMeasure10], 
+            [process_data.drinks[0].strIngredient11, process_data.drinks[0].strMeasure11], 
+            [process_data.drinks[0].strIngredient12, process_data.drinks[0].strMeasure12], 
+            [process_data.drinks[0].strIngredient13, process_data.drinks[0].strMeasure13],
+            [process_data.drinks[0].strIngredient14, process_data.drinks[0].strMeasure14],
+            [process_data.drinks[0].strIngredient15, process_data.drinks[0].strMeasure15], 
+          ],
           photo : process_data.drinks[0].strDrinkThumb,
           error : ""
         });
@@ -83,6 +120,7 @@ class App extends React.Component{
         <Cocktail 
         nameOfDrink={this.state.nameOfDrink}
         method={this.state.method}
+        ingredients={this.state.ingredients}
         photo={this.state.photo}
         error = {this.state.error}
         />
--- src/components/Cocktail/Cocktail.js
+++ src/components/Cocktail/Cocktail.js
@@ -4,6 +4,24 @@ class Cocktail extends React.Component{
         str = str.replace(/(\S\.|[!])\s*([A-Z])/g, "$1\n$2");
         return str;
     }
+
+    getIngredients(ingredients) {
+        let string = "";
+        let count = 1;
+        for (var i = 0; i < ingredients.length; i++) {
+            if(ingredients[i][0] != null && ingredients[i][0]!=""){
+                if(ingredients[i][1] != null && ingredients[i][1]!=""){
+                    string+= count.toString() + ". " + ingredients[i][0] + " (" + ingredients[i][1] + ") \n"
+                } else { 
+                    string+= count.toString() + ". " + ingredients[i][0] + "\n"
+                }
+                count++;
+            } else {
+                return string;
+            }
+        }
+        return string;
+    }
     render(){
         return(
             <div class="cocktail-wrapper">
@@ -14,6 +32,9 @@ class Cocktail extends React.Component{
                     </div>
                     <div class="cocktail-wrapper-column2">
                         {this.props.method && <p> Instructions <br></br><span>{this.getMethod(this.props.method)}</span></p>}
+                    </div>
+                    <div class="cocktail-wrapper-column2 ingredients">
+                        {this.props.ingredients && <p> Ingredients <br></br><span>{this.getIngredients(this.props.ingredients)}</span></p>}
                     </div>
                 </div>
                 <div>
